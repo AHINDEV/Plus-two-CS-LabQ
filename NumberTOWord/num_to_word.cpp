ec9c9524daa2345e1c7a372fe48f638559ce309c
AHINDEV <ahindev20@gmail.com>
2026-02-02 05:46:56 +0530

    Update num_to_word.cpp

--- NumberTOWord/num_to_word.cpp
+++ NumberTOWord/num_to_word.cpp
@@ -1,22 +1,21 @@
 #include<iostream>
 using namespace std;
-
 int main(){
-    string num;
+    int num;
     cout<<"Enter a Number :";
     cin>>num;
-
-    switch(num[0]){
-        case '0': cout<<"zero"; break;
-        case '1': cout<<"one"; break;
-        case '2': cout<<"two"; break;
-        case '3': cout<<"three"; break;
-        case '4': cout<<"four"; break;
-        case '5': cout<<"five"; break;
-        case '6': cout<<"six"; break;
-        case '7': cout<<"seven"; break;
-        case '8': cout<<"eight"; break;
-        case '9': cout<<"nine"; break;
-        default: cout<<"invalid digit"; break;
+    switch(num){
+        case 0:cout<<"zero";break;
+        case 1:cout<<"One";break;
+        case 2:cout<<"Two";break;
+        case 3:cout<<"Three";break;
+        case 4:cout<<"four";break;
+        case 5:cout<<"five";break;
+        case 6:cout<<"six";break;
+        case 7:cout<<"seven";break;
+        case 8:cout<<"eight";break;
+        case 9:cout<<"nine";break;
+        default:cout<<"invalid digit";break;
     }
+
 }

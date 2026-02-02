b9c3e7d411e5b83169b8aee7c12e7b4e666272a6
AHINDEV <ahindev20@gmail.com>
2026-02-02 05:38:15 +0530

    Refactor num_to_word to use string input
    Updated the program to accept string input for numbers and handle invalid digits.

--- NumberTOWord/num_to_word.cpp
+++ NumberTOWord/num_to_word.cpp
@@ -1,20 +1,22 @@
 #include<iostream>
 using namespace std;
+
 int main(){
-    int num;
-    cout<<"Enter Your Number :";
+    string num;
+    cout<<"Enter a Number :";
     cin>>num;
-    switch(num){
-        case 0 :cout<<"Number is zero ";break;
-        case 1 :cout<<"Number is One ";break;
-        case 2 :cout<<"Number is Two ";break;
-        case 3 :cout<<"Number is Three ";break;
-        case 4 :cout<<"Number is Four ";break;
-        case 5 :cout<<"Number is five ";break;
-        case 6 :cout<<"Number is six ";break;
-        case 7 :cout<<"Number is seven ";break;
-        case 8 :cout<<"Number is eight";break;
-        case 9 :cout<<"Number is nine ";break;
-        default :cout<<"Invalid Digit ";
+
+    switch(num[0]){
+        case '0': cout<<"zero"; break;
+        case '1': cout<<"one"; break;
+        case '2': cout<<"two"; break;
+        case '3': cout<<"three"; break;
+        case '4': cout<<"four"; break;
+        case '5': cout<<"five"; break;
+        case '6': cout<<"six"; break;
+        case '7': cout<<"seven"; break;
+        case '8': cout<<"eight"; break;
+        case '9': cout<<"nine"; break;
+        default: cout<<"invalid digit"; break;
     }
 }
